c5426208573a38f03c2e5fa65b56b36640890e14
bvpav <bvpav@tutanota.com>
2021-03-27 22:55:56 +0200

    Implement spa server

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express = require('express');
 
 app = express();
 
-app.use('/', express.static(path.resolve(__dirname, 'frontend')));
+app.use('/static', express.static(path.resolve(__dirname, 'frontend', 'static')));
+
+app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'index.html')));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`running on http://localhost:${port}/`));
