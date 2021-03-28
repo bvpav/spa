c8d68cade08b2ee82b812a0c2d66d4dbf27a4151
bvpav <bvpav@tutanota.com>
2021-03-28 17:10:04 +0300

    Add stuff to settings view

--- frontend/static/js/views/Settings.js
+++ frontend/static/js/views/Settings.js
@@ -8,8 +8,20 @@ export default class extends AbstractView {
 
   async getHTML() {
     return `
-            <h1>Posts</h1>
-            <p>Manage your settings</p>
+      <h1>Settings</h1>
+
+      <article>
+        <input type="checkbox" name="foobar" id="foobar" />
+        <label for="foobar">Use Foobar</label>
+      </article>
+
+      <article>
+        <label for="theme">Theme</label>
+        <select name="theme" id="theme">
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+        </select>
+      </article>
     `;
   }
 }
