5efc8ddf120531c0e43334e53ef55a15907a4b77
bvpav <bvpav@tutanota.com>
2021-03-28 13:57:29 +0300

    Implement view params

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -2,6 +2,15 @@ import Dashboard from './views/Dashboard.js';
 import Posts from './views/Posts.js';
 import Settings from './views/Settings.js';
 
+const pathToRegex = path => new RegExp('^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + '$');
+
+const getParams = (match) => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map((result) => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => [key, values[i]]));
+};
+
 const navigateTo = url => {
     history.pushState(null, null, url);
     router();
@@ -11,20 +20,28 @@ const router = async () => {
     const routes = [
         { path: '/', view: Dashboard },
         { path: '/posts', view: Posts },
+        { path: '/posts/:id', view: Posts },
         { path: '/settings', view: Settings },
     ];
 
     // test each route for potential matches
-    const matches = routes.map(route => {
+    const potentialMatches = routes.map(route => {
         return {
             route,
-            isMatch: location.pathname === route.path,
-        }
+            result: location.pathname.match(pathToRegex(route.path)),
+        };
     });
 
-    const match = matches.find(m => m.isMatch) || { route: routes[0], isMatch: true };
+    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
 
-    const view = new match.route.view();
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true,
+        }
+    }
+
+    const view = new match.route.view(getParams(match));
 
     document.querySelector('#app').innerHTML = await view.getHTML();
 };
